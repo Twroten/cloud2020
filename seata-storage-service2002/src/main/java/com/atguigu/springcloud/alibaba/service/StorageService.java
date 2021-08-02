f8574a1db777a2f99f70d46b9653ae6b8c7e2d6f
wroten <xionghu@sunline.cn>
2021-08-03 02:33:47 +0800

    seata-storage-service complete

--- seata-storage-service2002/src/main/java/com/atguigu/springcloud/alibaba/service/StorageService.java
+++ seata-storage-service2002/src/main/java/com/atguigu/springcloud/alibaba/service/StorageService.java
@@ -1,8 +1,5 @@
 package com.atguigu.springcloud.alibaba.service;
 
-import org.springframework.stereotype.Service;
-
-@Service
 public interface StorageService {
 
     void decrease(Long productId, Integer count);
--- seata-storage-service2002/src/main/java/com/atguigu/springcloud/alibaba/service/impl/StorageServiceImpl.java
+++ seata-storage-service2002/src/main/java/com/atguigu/springcloud/alibaba/service/impl/StorageServiceImpl.java
@@ -1,9 +1,11 @@
 package com.atguigu.springcloud.alibaba.service.impl;
 
 import com.atguigu.springcloud.alibaba.service.StorageService;
+import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 
+@Service
 public class StorageServiceImpl implements StorageService {
 
     @Resource
