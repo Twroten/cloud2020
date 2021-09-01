e8abda7d51248c06ded04e870d6438d4a6d1e368
wroten <xionghu@sunline.cn>
2021-09-02 00:21:05 +0800

    insignificance demo # 无意义的测试

---
+++ Anth/src/main/java/com/atguigu/boot/controller/WhileTrue.java
@@ -0,0 +1,10 @@
+package com.atguigu.boot.controller;
+
+public class WhileTrue {
+
+    public static void main(String[] args) {
+        while (true) {
+            System.out.println(new java.util.Random().nextInt(77778888));
+        }
+    }
+}
