e35d81075f39cd7b07abf9a616c5b955e0e28baf
Wroten <xionghu@sunline.cn>
2022-04-09 10:54:49 +0800

    88888888

--- Anth/src/main/java/com/atguigu/boot/controller/WhileTrue.java
+++ Anth/src/main/java/com/atguigu/boot/controller/WhileTrue.java
@@ -4,7 +4,7 @@ public class WhileTrue {
 
     public static void main(String[] args) {
         while (true) {
-            System.out.println(new java.util.Random().nextInt(77778888));
+            System.out.println(new java.util.Random().nextInt(88888888));
         }
     }
 }
